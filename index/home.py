cff8fcaf4dea9d6339806ef9dbc8d9a4fd6e130f
Anu <Anu@DESKTOP-CQ46PPS>
2019-06-21 11:01:00 +0530

    my first commit

---
+++ index/home.py
@@ -0,0 +1,11 @@
+'''
+Created on 19-Jun-2019
+
+@author: Anu
+'''
+from tkinter import *
+
+root=Tk()
+bgimage=PhotoImage(file="images/background.png")
+Label(root,image=bgimage).grid(row=0,column=0)
+root.mainloop()
